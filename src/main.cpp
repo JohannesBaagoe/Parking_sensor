3c5f8b7ad85664a178d338996827311e7ccbbb0e
JohannesBaagoe <cooliguy11@gmail.com>
2024-09-18 11:23:04 +0200

    merged

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 
 ToF sensor1;
+const char* ssid = "Not_Russian_spies";
+const char* password = "Diller123";
 
 
 void handleInterrupt() {
@@ -24,7 +26,21 @@ void setup() {
 }
 
 void loop() {
-       
+
+    WiFiClient espClient;
+    mqtt_com mqtt_sender(espClient);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print('.');
+      delay(1000);
+    }
+    while(!mqtt_sender.connect()){
+      Serial.println("Trying to connect to server");
+      delay(500);
+    }
+
     sensor1 = ToF(1);
 
     if(sensor1.SetInteruptThresholds(100, 500) == VL53L0X_ERROR_NONE){
@@ -38,36 +54,8 @@ void loop() {
       Serial.print("Distance (mm): ");
       uint16_t distance = sensor1.getMeasurement();
       Serial.println(distance);
+      mqtt_sender.send_sensor_information();
+      delay(100);
 
     }
-}
-
-
-const char* ssid = "Not_Russian_spies";
-const char* password = "Diller123";
-
-void setup() {
-
-
-}
-
-void loop() {
-  WiFiClient espClient;
-  mqtt_com mqtt_sender(espClient);
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-
-  while(!mqtt_sender.connect()){
-    Serial.println("Trying to connect to server");
-    delay(500);
-  }
-  while(1){
-    mqtt_sender.send_sensor_information();
-    delay(100);
-  }
 }
